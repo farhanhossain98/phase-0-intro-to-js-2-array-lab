f81166af5cdcaab3c4ea6e0ca9d7e03910a2ca27
farhan hossain <farhanrufon99@gmail.com>
2023-07-01 22:31:11 -0400

    changes index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo","Otis", "Garfield"];
+const copyofcats = [...cats];
+function destructivelyAppendCat() {
+cats.push("Ralph");
+}
+const copyofcats2 = [...cats];
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+const copyofcats3 = [...cats];
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+const copyofcats4 = [...cats];
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+   const newcats= [...cats, name] 
+return newcats;
+}
+function prependCat(name) {
+    const newcats = [name, ...cats]
+    return newcats
+}
+function removeLastCat(name) {
+    return cats.slice(0,-1);
+} 
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
